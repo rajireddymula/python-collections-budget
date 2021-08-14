339aed16dea0a666461de75ff1e66174437d9550
rajireddymula <rajireddym@live.com>
2021-08-14 14:18:49 +0100

    finaltouch

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -21,7 +21,7 @@ class BudgetList():
             self.sum_overages = self.sum_overages + item
 
     def __len__(self):
-        return[len(self.expenses) + len(self.overages)]
+        return len(self.expenses) + len(self.overages)
 
 
 def main():
